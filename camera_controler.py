3d744d8bb1b685cd3d3ff14a01dcd827fc1eb8a4
msahmed1 <shabaj_ahmed@outlook.com>
2024-02-25 22:26:42 +0000

    Add MQTT functionality followed by publishing movement states

--- camera_controler.py
+++ camera_controler.py
@@ -1,17 +1,34 @@
-# import, set up 'correct' paths for silicon macs 
+# import, set up 'correct' paths for silicon macs
+import mediapipe as mp
+import cv2
 import os
+import paho.mqtt.client as mqtt
+import threading
 home_dir = os.path.expanduser("~")
 camera_optn = 0
 
 if home_dir == "/Users/nat":
     mediapipe_dylibs_path = "/Users/nat/Documents/coding/bris_hack_2024/venv/lib/python3.9/site-packages/mediapipe/.dylibs"
-    os.environ["DYLD_LIBRARY_PATH"] = mediapipe_dylibs_path + ":" + os.environ.get("DYLD_LIBRARY_PATH", "")
+    os.environ["DYLD_LIBRARY_PATH"] = mediapipe_dylibs_path + \
+        ":" + os.environ.get("DYLD_LIBRARY_PATH", "")
     camera_optn = 1
 
-import cv2
-import mediapipe as mp
-import numpy as np
-import time
+# MQTT setup
+broker_address = 'localhost'  # Define broker address
+port = 1883  # Default MQTT port
+keepalive = 60  # Keepalive interval
+
+mqtt_client = mqtt.Client()  # Create MQTT client
+# Connect to the MQTT broker
+mqtt_client.connect(broker_address, port, keepalive)
+# Start the loop in a separate thread
+threading.Thread(target=mqtt_client.loop_forever).start()
+mqtt_client.subscribe("robot/userInput")
+
+
+def publish_state(message):
+    mqtt_client.publish("robot/userInput", message)
+
 
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
@@ -24,14 +41,15 @@ jump_counter = 0
 previous_frame_crouch = False
 crouch_counter = 0
 
+
 def draw_connections(image, landmarks):
     # Render detections
     mp_drawing.draw_landmarks(image, landmarks, mp_pose.POSE_CONNECTIONS,
-                        mp_drawing.DrawingSpec(
-                            color=(245, 117, 66), thickness=2, circle_radius=2),
-                        mp_drawing.DrawingSpec(
-                            color=(245, 66, 230), thickness=2, circle_radius=2)
-                        )
+                              mp_drawing.DrawingSpec(
+                                  color=(245, 117, 66), thickness=2, circle_radius=2),
+                              mp_drawing.DrawingSpec(
+                                  color=(245, 66, 230), thickness=2, circle_radius=2)
+                              )
 
 
 def detect_movement(landmarks):
@@ -41,9 +59,10 @@ def detect_movement(landmarks):
     shoulder_y = (landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y +
                   landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y) / 2
 
-    # Detect jump (significant upward movement)
+    # Detect jump
     if not jump_in_progress and previous_shoulder_y - shoulder_y > 0.05:  # Threshold for jump start
         jump_in_progress = True
+        publish_state('up')
     elif jump_in_progress and shoulder_y - previous_shoulder_y > 0.05:  # Threshold for jump end
         jump_in_progress = False
         jump_counter += 1
@@ -56,14 +75,23 @@ def detect_movement(landmarks):
     left_knee_y = landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y
     right_knee_y = landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y
 
-    # Check if either wrist is below either knee
+    # Detect crouch when wrist is below either knee
     crouch_detected = left_wrist_y > left_knee_y or right_wrist_y > right_knee_y
 
-    # Detect crouch as soon as it happens
     if crouch_detected and not previous_frame_crouch:
         crouch_counter += 1
+        publish_state('down')
 
     previous_frame_crouch = crouch_detected
+
+    hip_y = (landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y +
+             landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y) / 2
+    knee_y = (landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y +
+              landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y) / 2
+
+    # and (knee_y - shoulder_y) > some_threshold:
+    if shoulder_y < hip_y < knee_y and not jump_in_progress:
+        publish_state('standing')
 
     return jump_counter, crouch_counter
 
@@ -93,19 +121,19 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
         try:
             landmarks = results.pose_landmarks.landmark
 
-            jumps, crouches = detect_movement(landmarks)
+            detect_movement(landmarks)
 
         except:
             pass
 
         # Display jump and crouch count
-        cv2.putText(image, f'Jumps: {jumps}', (10, 50),
+        cv2.putText(image, f'Jumps: {jump_counter}', (10, 50),
                     cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-        cv2.putText(image, f'Crouches: {crouches}', (10, 100),
+        cv2.putText(image, f'Crouches: {crouch_counter}', (10, 100),
                     cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
         draw_connections(image, results.pose_landmarks)
-        
+
         cv2.imshow('Mediapipe Feed', image)
 
         if cv2.waitKey(10) & 0xFF == ord('q'):
